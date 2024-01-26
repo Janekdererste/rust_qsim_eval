630c24725580ece40b1eb09d2afaaac218cc725b
janekdererste <janek.laudan@mailbox.org>
2024-01-26 20:09:28 +0100

    improve graphs

--- src/main/R/eval_runtime.R
+++ src/main/R/eval_runtime.R
@@ -8,15 +8,33 @@ source("./src/main/R/parsing.R")
 source("./src/main/R/tracing.R")
 
 
-traces <- load_csv_instrument("/Users/janek/Documents/rust_q_sim/equil/output", num_cores = 8)
+traces <- load_csv_instrument("/Users/janek/Cluster/pqsim_benchmark/berlin-v6.0-25pct/output/", num_cores = 8)
 overall_run_time <- traces %>%
   filter(func == "rust_q_sim::simulation::simulation::run") %>%
   mutate (speedup = (1/duration) / (1/max(duration))) %>%
   mutate(secs = duration / 1e9)
 
-p <- ggplot(overall_run_time, aes(x = size, y = secs)) +
+duration_summary <- overall_run_time %>%
+  group_by(size) %>%
+  summarize(mean_duration = mean(secs))
+
+speedup_summary <- overall_run_time %>%
+  group_by(size) %>%
+  summarize(mean_speedup = mean(speedup))
+
+p <- ggplot(duration_summary, aes(x = size, y = mean_duration)) +
+  geom_line(color = blue()) +
   geom_point(color = blue()) +
-  stat_summary(fun = mean, color = blue(), geom = "line") +
+  geom_text(aes(label = round(mean_duration, 1)), vjust = -0.5, hjust = -0.05) +
+  ggtitle("Overall Runtime [s]") +
+  theme_light()
+p
+
+p <- ggplot(speedup_summary, aes(x = size, y = mean_speedup)) +
+  geom_line(color = blue()) +
+  geom_point(color = blue()) +
+  geom_text(aes(label = round(mean_speedup, 1)), vjust = 1.5, hjust = -0.1) +
+  ggtitle("Overall Speedup") +
   theme_light()
 p
 
