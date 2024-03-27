122ebfbbb88cf252c21c77d377f4d170d7a53b65
janekdererste <janek.laudan@mailbox.org>
2024-03-27 11:44:21 +0100

    re-order categories, and colors

--- src/main/R/eval_acc_times.R
+++ src/main/R/eval_acc_times.R
@@ -49,11 +49,18 @@ data_8192 <- read_binary_tracing_files(roots =
   summarize(mean_dur = mean(duration), sum_dur = sum(duration))
 
 data <- data %>%
-  bind_rows(data_8192) %>%
-  mutate(category = if_else(size > 256, "large", "small"))
+  bind_rows(data_8192)
+
+order <- c("receive_msgs", "send_msgs", "handle_msgs", "move_links", "move_nodes", "wakeup", "terminate_teleportation")
+colors_msging <- c("#feb70e", "#fece5a", "#fee5a6")
+colors_work <- c("#0e54fe", "#4178fe", "#739cfe", "#a6c0fe")
+ordered_data <- data %>%
+  mutate(category = if_else(size > 256, "large", "small")) %>%
+  mutate(category = fct_relevel(category, c("small", "large"))) %>%
+  mutate(func = fct_relevel(func, order))
 
 
-p1 <- ggplot(data, aes(x = as.factor(size), y = mean_dur / 1e3, fill = as.factor(func))) +
+p1 <- ggplot(ordered_data, aes(x = as.factor(size), y = mean_dur / 1e3, fill = as.factor(func))) +
   geom_bar(stat = "identity", position = "stack") +
   #scale_y_log10() +
   facet_wrap(~category, scales = "free") +
@@ -61,11 +68,11 @@ p1 <- ggplot(data, aes(x = as.factor(size), y = mean_dur / 1e3, fill = as.factor
   ylab("Mean Duration [\u00B5s]") +
   labs(fill = "Functions") +
   ggtitle("Mean duration of performing one sim step") +
-  scale_fill_manual(values = qualitative()) +
+  scale_fill_manual(values = c(colors_msging, colors_work)) +
   theme_light()
 p1
 
-work <- data %>% filter(
+work <- ordered_data %>% filter(
   func == "wakeup" |
     func == "terminate_teleportation" |
     func == "move_nodes" |
@@ -74,11 +81,12 @@ work <- data %>% filter(
 
 p2 <- ggplot(work, aes(x = as.factor(size), y = mean_dur / 1e3, fill = as.factor(func))) +
   geom_bar(stat = "identity", position = "stack") +
+  facet_wrap(~category, scales = "free") +
   xlab("Number of Cores") +
   ylab("Mean Duration [\u00B5s]") +
   labs(fill = "Functions") +
   ggtitle("Mean duration of performing simulation work for one simulation time step") +
-  scale_fill_manual(values = qualitative()) +
+  scale_fill_manual(values = colors_work) +
   theme_light()
 p2
 
@@ -94,9 +102,10 @@ p3 <- ggplot(messaging, aes(x = as.factor(size), y = mean_dur / 1e3, fill = as.f
   ylab("Mean Duration [\u00B5s]") +
   labs(fill = "Functions") +
   ggtitle("Mean duration of performing messaging for one simulation time step") +
-  scale_fill_manual(values = qualitative()) +
+  scale_fill_manual(values = colors_msging) +
   theme_light()
 p3
 
 combined <- p1 / p2 / p3
+combined
 ggsave("mean-dur.pdf", combined, device = "pdf", width = 210, height = 297, units = "mm")
